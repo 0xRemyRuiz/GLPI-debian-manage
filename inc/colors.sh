b8d12d6bf63d46e857147dc7e5b41713e7faf7ce
0xRemyRuiz <remyruiz@protonmail.com>
2026-03-19 00:27:23 +0100

    added missing file and fixed install issues

---
+++ inc/colors.sh
@@ -0,0 +1,14 @@
+
+CRST='\e[0m'
+
+Red='\e[0;31m'
+Green='\e[0;32m'
+Yellow='\e[0;33m'
+Blue='\e[0;34m'
+Purple='\e[0;35m'
+
+BRed='\e[1;31m'
+BGreen='\e[1;32m'
+BYellow='\e[1;33m'
+BBlue='\e[1;34m'
+BPurple='\e[1;35m'
